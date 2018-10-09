5af5f5bf10e71e4acbc56d3b1788de4257571d98
Banu Akkuş <akkusbanu92@gmail.com>
2018-10-09 08:11:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function happyHolidayTo(holiday,name) {
 }
 
 function holidayCountdown(days, holiday) {
- var message3 ='It\'s ${days} days until ${holiday}!'
+ var message3 =`It\'s ${days} days until ${holiday}!`
  return (message3) 
 }
